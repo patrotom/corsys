9e69d153bdd31ceaccd6b233bf8de39001f252f2
Peter <kolarp33@fit.cvut.cz>
2018-11-24 15:07:14 +0100

    CreateReservationCommand added, Receptionist name after login, ReservationController started

---
+++ src/main/java/cz/cvut/fit/corsys/pl/web/command/CreateReservationCommand.java
@@ -0,0 +1,95 @@
+package cz.cvut.fit.corsys.pl.web.command;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class CreateReservationCommand {
+
+    @NotNull
+    private Integer departmentId;
+
+    @NotNull
+    private Integer examinationId;
+
+    @NotNull
+    private Integer doctorId;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    private LocalTime timeFrom;
+
+    @NotNull
+    private LocalTime timeTo;
+
+    @NotNull
+    private  String state;
+
+    private String description;
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getExaminationId() {
+        return examinationId;
+    }
+
+    public void setExaminationId(Integer examinationId) {
+        this.examinationId = examinationId;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Integer doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTimeFrom() {
+        return timeFrom;
+    }
+
+    public void setTimeFrom(LocalTime timeFrom) {
+        this.timeFrom = timeFrom;
+    }
+
+    public LocalTime getTimeTo() {
+        return timeTo;
+    }
+
+    public void setTimeTo(LocalTime timeTo) {
+        this.timeTo = timeTo;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/cz/cvut/fit/corsys/pl/web/controller/ReservationController.java
+++ src/main/java/cz/cvut/fit/corsys/pl/web/controller/ReservationController.java
@@ -1,18 +1,31 @@
 package cz.cvut.fit.corsys.pl.web.controller;
 
 
+import cz.cvut.fit.corsys.bl.service.*;
+import cz.cvut.fit.corsys.dl.entity.Department;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReservationController {
 
+    @Autowired
+    private DepartmentService departmentService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private DoctorService doctorService;
 
     @RequestMapping(value = "/receptionist/createReservation", method = RequestMethod.GET)
     public String prepareCreateReservation(Model model) {
-        model.addAttribute("userFullName", "user name");
+        model.addAttribute("Departments", departmentService.findAllDepartments());
+        //model.addAttribute("Examinations", departmentService.findExaminations());
         return "receptionist/createReservation";
     }
 }
